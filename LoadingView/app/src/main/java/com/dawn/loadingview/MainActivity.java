8ed2fb22d33f476f8d13b501608119c6a2b25324
Bruce <911208318@qq.com>
2018-08-14 09:55:36 +0800

    添加数字进度条

--- LoadingView/app/src/main/java/com/dawn/loadingview/MainActivity.java
+++ LoadingView/app/src/main/java/com/dawn/loadingview/MainActivity.java
@@ -8,17 +8,26 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView rotating_loading;
+    TextView number_progress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         rotating_loading = findViewById(R.id.rotating_loading);
+        number_progress = findViewById(R.id.number_progress);
 
         rotating_loading.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this,ShowLoadingActivity.class);
+                startActivity(intent);
+            }
+        });
+        number_progress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,NumberProgressActivity.class);
                 startActivity(intent);
             }
         });
---
+++ LoadingView/app/src/main/java/com/dawn/loadingview/NumberProgressActivity.java
@@ -0,0 +1,55 @@
+package com.dawn.loadingview;
+
+import android.animation.ObjectAnimator;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.dawn.loadingview.view.NumberProgressBar;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by dawn on 2018/8/13.
+ */
+
+public class NumberProgressActivity extends AppCompatActivity {
+    private NumberProgressBar bnp;
+    private Timer timer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_number_progress);
+        bnp = findViewById(R.id.numberbar1);
+
+        bnp.setOnProgressBarListener(new NumberProgressBar.OnProgressBarListener() {
+            @Override
+            public void onProgressChange(int current, int max) {
+                bnp.setProgress(0);
+            }
+        });
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        bnp.incrementProgressBy(1);
+                    }
+                });
+            }
+        }, 1000, 100);
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        timer.cancel();
+    }
+    
+}
